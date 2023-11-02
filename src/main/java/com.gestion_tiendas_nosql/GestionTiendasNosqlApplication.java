146c659af348e5199d1497e6c53e96ba4dd806b9
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2023-11-02 16:34:46 -0500

    properties config

--- src/main/java/com.gestion_tiendas_nosql/GestionTiendasNosqlApplication.java
+++ src/main/java/com.gestion_tiendas_nosql/GestionTiendasNosqlApplication.java
@@ -1,9 +1,15 @@
-package com.gestiontiendas.gestion_tiendas_nosql;
+package com.gestion_tiendas_nosql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:application_secret.properties")
+})
 public class GestionTiendasNosqlApplication {
 
 	public static void main(String[] args) {
