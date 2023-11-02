3d5db439dfc5efe58445968944d89acc64717b84
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2023-11-02 16:44:05 -0500

    initial entities

---
+++ src/main/java/com.gestion_tiendas_nosql/Entities/Client.java
@@ -0,0 +1,15 @@
+package com.gestion_tiendas_nosql.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "Clients")
+public class Client {
+}
