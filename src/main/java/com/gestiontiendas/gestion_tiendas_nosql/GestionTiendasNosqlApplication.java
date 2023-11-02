9362283f4233c0c8056bf34154578e62dbeaa242
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2023-11-02 16:01:51 -0500

    Initial Commit

---
+++ src/main/java/com/gestiontiendas/gestion_tiendas_nosql/GestionTiendasNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.gestiontiendas.gestion_tiendas_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTiendasNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTiendasNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestiontiendas/gestion_tiendas_nosql/GestionTiendasNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiontiendas.gestion_tiendas_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTiendasNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
